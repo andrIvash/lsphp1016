3fd552a18325a093e81539e900411153a7602d2c
mvoevodskiy <mv@aaa0.ru>
2016-11-01 18:53:31 +0300

    PSR-2

--- index.php
+++ index.php
@@ -6,7 +6,7 @@ echo " 1 ___________________________________________________________\n\n";
 $name = 'андрей иващенко';
 $age = '36';
 echo "Меня зовут $name \n";
-echo 'Мне '.$age.' лет'.PHP_EOL;
+echo 'Мне ' . $age . ' лет' . PHP_EOL;
 echo "\"!|/'\"/\n";
 
 echo "\n\n";
@@ -17,7 +17,7 @@ echo " 2 ___________________________________________________________\n\n";
 $all = 80;
 $felt = 23;
 $pencil = 40;
-echo $paints =  $all - $felt - $pencil.PHP_EOL;
+echo $paints = $all - $felt - $pencil . PHP_EOL;
 
 echo "\n\n";
 
@@ -25,7 +25,7 @@ echo "\n\n";
 // Не полностью соответствует заданию
 echo " 3 ___________________________________________________________\n\n";
 define("ROUND", 360);
-echo ROUND.PHP_EOL;
+echo ROUND . PHP_EOL;
 // const ROUND = 280;
 
 echo "\n\n";
@@ -34,11 +34,11 @@ echo "\n\n";
 // Принято
 echo " 4 ___________________________________________________________\n\n";
 $age = 36;
-if ($age >= 18 && $age <=65) {
+if ($age >= 18 && $age <= 65) {
     echo "Вам еще работать и работать \n";
 } elseif ($age > 65) {
     echo "Вам пора на пенсию \n";
-} elseif ($age >= 1 && $age <=17) {
+} elseif ($age >= 1 && $age <= 17) {
     echo "Вам ещё рано работать \n";
 } else {
     echo "Неизвестный возраст \n";
@@ -50,7 +50,7 @@ echo "\n\n";
 //#5
 // Принято
 echo " 5 ___________________________________________________________\n\n";
-$day  = 5;
+$day = 5;
 switch ($day) {
     case ($day > 0 && $day < 6):
         echo "Это рабочий день";
@@ -71,29 +71,29 @@ $bmw = [
     "model" => "X5",
     "speed" => 120,
     "doors" => 5,
-    "year"  =>  "2015"
+    "year" => "2015"
 ];
 $toyota = [
     "model" => "venza",
     "speed" => 100,
     "doors" => 5,
-    "year"  =>  "2014"
+    "year" => "2014"
 ];
 $opel = [
     "model" => "astra",
     "speed" => 120,
     "doors" => 5,
-    "year"  =>  "2010"
+    "year" => "2010"
 ];
 
 $result = array_merge_recursive($bmw, $toyota, $opel);
-print_r($result).PHP_EOL;
+print_r($result) . PHP_EOL;
 
 $cars = ["CAR BMW", "CAR TOYOTA", "CAR OPEL"];
 for ($i = 0; $i < count($cars); $i++) {
-    echo  $cars[$i].PHP_EOL;
+    echo $cars[$i] . PHP_EOL;
     foreach ($result as $value) {
-        echo  $value[0].' ';
+        echo $value[0] . ' ';
     }
     echo "\n\n";
 
@@ -108,11 +108,11 @@ for ($i = 1; $i <= 10; $i++) {
     echo "<tr>";
     for ($j = 1; $j <= 10; $j++) {
         if ($i % 2 === 0 && $j % 2 === 0) {
-            echo "<td>[".($i*$j)."]</td>";
+            echo "<td>[" . ($i * $j) . "]</td>";
         } elseif (($i % 2 !== 0 && $j % 2 !== 0)) {
-            echo "<td>(".($i*$j).")</td>";
+            echo "<td>(" . ($i * $j) . ")</td>";
         } else {
-            echo "<td>&nbsp;".($i*$j)."&nbsp;</td>";
+            echo "<td>&nbsp;" . ($i * $j) . "&nbsp;</td>";
         }
     }
     echo "</tr>";
@@ -128,11 +128,11 @@ echo " 8 ___________________________________________________________\n\n";
 echo $str = "Hello world of PHP";
 echo "\n";
 $arr = explode(" ", $str);
-print_r($arr).PHP_EOL;
+print_r($arr) . PHP_EOL;
 
 $end = end($arr);
 while ($end) {
-    echo $end.' - ';
+    echo $end . ' - ';
     $end = prev($arr);
 }
 
